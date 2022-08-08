1350f6b8855e3c4d68e0cbe665d2300daccf7d3a
Yuval Ben-Naim <yuval.bennaim@datarobot.com>
2022-08-07 18:48:26 -0600

    example

---
+++ examples/webcomponents_node/client/js/sample-app.js
@@ -0,0 +1,26 @@
+import SimpletonStateManager from "./SimpletonStateManager.js";
+
+class comp extends HTMLElement {
+    
+  connectedCallback() {
+    this.attachShadow({mode: 'open'});
+    
+    //http://localhost:3000/data
+    //get sample data from API
+    //store in stateStore
+
+    const store = new SimpletonStateManager(); 
+    const modelTwo = { name: 'modelTwo', value: 2 };
+    store.setModel('modelTwo', modelTwo);
+    const models = store.getModelList();
+    console.log('Models', models);
+    this.render();
+  }
+
+  render() {
+      this.shadowRoot.innerHTML = `<div><h2>Hello from within the SampleApp!</h2></div>`;
+      //render display CustomElements
+  }
+}
+
+customElements.define('sample-app', comp);
